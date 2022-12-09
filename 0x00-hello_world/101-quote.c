fa5d329e2adf8ae0bf43a640e07da939f529680e
lizzyraesetja <lizzraesetja2@gmail.com>
2022-12-09 01:11:46 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - EntryPoint
+ * Return: program should return 1
+ */
+int main(int)
+{
+	putchar("\"and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return(1);
+}
