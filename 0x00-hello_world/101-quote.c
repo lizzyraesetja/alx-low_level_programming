27f9fea6c3f5eaf655e92333bc467d6eda029226
lizzyraesetja <lizzraesetja2@gmail.com>
2022-12-09 01:35:17 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- * main - EntryPoint
- * Return: program should return 1
+#include <unistd.h>                               /**
+ * main - EntryPoint                               * Return: This program must return 1
  */
-int main(int)
+int main(void)
 {
-	putchar("\"and that piece of art is useful" - Dora Korpar, 2015-10-19");
+        write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return(1);
 }
