75e3dc7cdc40f2478c1e14720efc6cee7dad79fd
lizzyraesetja <lizzraesetja2@gmail.com>
2022-12-14 15:34:36 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the even-valued terms
+ * Return: Nothing!
+ */
+
+int main(void)
+{
+	int y = 0;
+	long int j = 1, k = 2;
+
+	while (y < 50)
+	{
+		if (y == 0)
+		{
+			printf("%d\n", j);
+		}
+		else if (y == 1)
+		{
+			printf(", %d\n", k);
+		}
+		else
+		{
+			k += j;
+			j = k -j;
+			printf(", %ld\n", k);
+		}
+		y++;
+	}
+	printf('\n');
+	return (0);
