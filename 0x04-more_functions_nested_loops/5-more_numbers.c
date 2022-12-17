bc4919dcd63fa148ff483098bc8c71d00d876353
lizzyraesetja <lizzraesetja2@gmail.com>
2022-12-16 17:57:31 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,13 @@ void more_numbers(void)
 {
 	int i, y;
 
-	for (y = 0; y <= 9; y++)
+	for (y = 0; y <= 10; y++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = 0; i <= 15; i++)
 		{
 			if (i > 9)
 			{
-				_putchar((i / 10) + '0');
+				_putchar('1');
 				_putchar((i % 10) + '0');
 			}
 			_putchar('\n');
