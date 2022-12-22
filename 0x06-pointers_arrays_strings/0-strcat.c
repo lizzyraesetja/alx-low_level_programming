35d64a2bf770f56318ed929dfaf2999d398db134
lizzyraesetja <lizzraesetja2@gmail.com>
2022-12-22 08:01:49 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int index = 0, dest_len = 0;
+	int x = 0, d_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[x++])
+		d_len++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (x = 0; src[x]; x++)
+		dest[d_len++] = src[x];
 
 	return (dest);
 }
