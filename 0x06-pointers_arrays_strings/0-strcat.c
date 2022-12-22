cd4ce19c3bf68fefe1e81dbc33ddba8d72e78a4f
lizzyraesetja <lizzraesetja2@gmail.com>
2022-12-22 11:36:17 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,20 +2,22 @@
 
 /**
  * _strcat - function that concatenates two strings
- * @dest: Append the string
- * @src: string to be concebtrated
- * Return: Returns a pointer to the resulting string
+ * @dest: Append string
+ * @src: concentrated string
+ *
+ * Return: Returns a pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int x = 0, dest_len = 0;
+	int y = 0, dest_len = 0;
 
-	while (dest[x++])
+	while (dest[y++])
 		dest_len++;
 
-	for (x = 0; src[x]; x++)
-		dest[dest_len++] = src[x];
+	for (y = 0; src[y]; y++)
+		dest[dest_len++] = src[y];
+
 	return (dest);
 }
 
