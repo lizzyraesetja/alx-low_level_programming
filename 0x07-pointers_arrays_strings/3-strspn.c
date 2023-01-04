9682b3d0d7816bb6db631e40f14294adf8e08e02
lizzyraesetja <lizzraesetja2@gmail.com>
2023-01-04 09:18:23 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,14 +10,14 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int index;
+	unsigned int bytes = 0;
+	int index;
 
-	while (*s)
+	while (*s)
 	{
-		for (index = 0; accept[index]; index++)
+		for (index = 0; accept[index]; index++)
 		{
-			if (*s == accept[index])
+			if (*s == accept[index])
 			{
 				bytes++;
 				break;
@@ -25,7 +25,7 @@ unsigned int _strspn(char *s, char *accept)
 			else if (accept[index + 1] == '\0')
 				return (bytes);
 		}
-		s++
+		s++;
 	}
-	return (bytes)
+	return (bytes);
 }
