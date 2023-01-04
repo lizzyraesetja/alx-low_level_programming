09f48d4aad6c5143baf6267a550a46727ca75bc1
lizzyraesetja <lizzraesetja2@gmail.com>
2023-01-04 11:30:50 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,24 +12,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int index;
-	if (*needle == 0)
+	int index;
+	if (*needle == 0)
 		return (haystack);
-	while (*haystack)
+	while (*haystack)
 	{
 		index = 0;
 	}
-	if (haystack[index] == needle[index])
+	if (haystack[index] == needle[index])
 		do {
-			if (needle[index + 1] == '\0')
-				return (haystack)
-				index++;
-		} while (haystack[index] == needle[index]);
-}
+			if (needle[index + 1] == '\0')
+			{
+				return (haystack);
+			}
+			index++;
+		} while (haystack[index] == needle[index]);
+	haystack++;
 
-haystack++;
-
-}
-
-return ('\0');
+	return ('\0');
 }
