b8fcae11b723a33e6dc36f6b96d6599733fa7000
lizzyraesetja <lizzraesetja2@gmail.com>
2023-01-04 10:18:09 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,14 +12,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int index;
 
-	while (*s)
+	while (*s)
 	{
-		for (index = 0; accept[index]; index++)
+		for (index = 0; accept[index]; index++)
 		{
-			if (*s == accept[index])
+			if (*s == accept[index])
 				return (s);
 		}
-		s++
+		s++;
 	}
 	return ('\0');
 }
