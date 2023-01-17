460bc01e1661a66205e9f1359480c6d0ac19c83d
lizzyraesetja <lizzraesetja2@gmail.com>
2023-01-17 10:35:32 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *p;
 	/* reserving memory to struct*/
 	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	if (p == NULL)
 		return (NULL);
 	/* Cpunting name pointer*/
 
